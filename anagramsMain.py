bc214938da149f5778058d824d2994dc2f2bbede
Jonathan Argumedo <43223141+cybert3xas@users.noreply.github.com>
2018-11-06 10:38:58 -0700

    Add files via upload

--- anagramsMain.py
+++ anagramsMain.py
@@ -1,12 +1,44 @@
+'''
+NAME: Jonathan Argumedo
+PROFESSOR NAME: Diego Aguirre 
+TA NAME: Anindita Nath  
+CLASS: CS 2302 Data Structures
+DATE: November 9, 2018 
+UPDATDE BY: (Your name goes here)
+'''
+
+
 from treesAVL import avlTree
 from treesAVL import Node
+from treesRed import redBlackTree
 
+#global tree (can be AVL or Red Black in this specific problem)
 global tree
 '''
-'populateTree' is a mehtod that takes a file name and a tree parameter and 
-populates the tree with the words from the textFile
+'countAnagram' is a method that returns the total number of anagrams of a word that 
+are found in a tree that has 354,984 English Words.
 '''
-def populateTree(fileName, tree):
+def countAnagram(word, treeOfWords,  listOfWords, prefix=""):
+    if len(word) <= 1:
+        str = prefix + word
+        currentNode = treeOfWords.search(str)
+        if currentNode is not None:
+            listOfWords.append(currentNode.key)
+    else:
+        for i in range(len(word)):
+            cur = word [i: i + 1]
+            before = word[0:i]
+            after = word[i +1:]
+            if cur not in before:
+                countAnagram(before + after, treeOfWords,  listOfWords, prefix + cur)
+    return len(listOfWords)#the number of anagrams found in the file are in a list
+                        #return the len of list and you'll have the anagrams total
+
+'''
+'populateAVLtree' is a mehtod that takes a file name and a tree parameter and 
+populates the tree with the words from the textFile (Uses AVL Tree)
+'''
+def populateAVLtree(fileName, tree):
     file = open(fileName, "r")
     for line in file:
         current_line = line.split()
@@ -14,25 +46,100 @@ def populateTree(fileName, tree):
             tree.insert(Node(current_line[0]))
         else:
             tree.insert(current_line[0])
+    file.close()
 
-#method provided by the PDF file
-            
-def print_anagrams(word, prefix=""):    
-    if len(word) <= 1:        
-        str = prefix + word 
-        current = tree.search(str)
-        if current is not None:            
-            print(current.key)    
-    else:        
+'''
+'populateRedTree' is a method that takes a file name and a tree parameter
+and populates the tree with words from the textFile (Uses Red  Black Tree)
+'''        
+def populateRedTree(fileName, tree):
+    file = open(fileName, "r")
+    for line in file:
+        temp = line.split()
+        tree.insert(temp[0])
+    file.close()
+'''
+'print_anagram' is amethod that generates all permutations of a word
+each output is verified by traversing a tree, this tree is populated
+with over 354,984 English Words. If a match is found then that word is an 
+anagram
+'''
+def print_anagram(word, treeOfWords, prefix=""):
+    if len(word) <= 1:
+        str = prefix + word
+        currentNode = treeOfWords.search(str)
+        if currentNode is not None:
+            print(currentNode.key)
+    else:
         for i in range(len(word)):
-            cur = word[i: i + 1]
-            before = word[0: i] # letters before cur            
-            after = word[i + 1:] # letters after cur
- 
-            if cur not in before: # Check if permutations of cur have not been generated.                
-                print_anagrams(before + after, prefix + cur) 
-            print_anagrams(word, prefix)
+            cur = word [i: i + 1]
+            before = word[0:i]
+            after = word[i +1:]
+            if cur not in before:
+                print_anagram(before + after, treeOfWords, prefix + cur)
+                
+'''
+'findMaxAnagrams' finds the maximum anagrams found in a file
+it takes a tree and a fileName as parameters
+'''
+def findMaxAnagrams(fileOfWords, tree):
+    finalMaxAnagrams = 0
+    wordFound = ""
+    
+    with open(fileOfWords, "r") as file:
+        for line in file:
+            #call the countAnagrams function that returns the anagrams of each word
+            #we call it with every word found in the file that is passed as parameter
+            #and the global tree that contains the 354, 984 words
+            listOfWords = []
+            currentMaxAnagrams = countAnagram(line.split()[0], tree, listOfWords)
+            
+            if currentMaxAnagrams > finalMaxAnagrams:#if the value returned is greater than the FinalMax then that means a new max has been found
+                finalMaxAnagrams = currentMaxAnagrams
+                wordFound = line.split()[0]
+        print("The word with the most anagrams is '%s' and it has [%d] anagrams." % (wordFound, finalMaxAnagrams))
+        print("\nThe anagrams of the word '%s' are..." % (wordFound))
+        print_anagram(wordFound, tree)
+        
+def main():
+    while True:
+        print("********************************************************************************")
+        option = input("Enter the tree you would like to use:\n\n1)AVL Tree\n2)Red Black Tree\n\nEnter response: ")
+        if option == '1' or option == "one":#check if the user selected AVL Tree
+            try:
+                tree = avlTree() #create an empty tree
+                print("Populating AVL Tree...")
+                populateAVLtree("words.txt", tree) #populate AVL Tree
+               
+                userWord = input("\nEnter the word you want to search: ")
+                print("\n\nThe anagrams fro the word '%s' are..." % (userWord))
+                print_anagram(userWord, tree) #check if the word has anagrams
+                
+            except FileNotFoundError:
+                print("\n\nOops! File not found!")
+                break
+            
+        #check if the option is an Red Black Tree
+        elif option == '2' or option == "two":
+            try:
+                tree = redBlackTree() #create an empty tree
+                print("Populating Red Black Tree...")
+                populateRedTree("words.txt", tree)
 
-tree = avlTree()
-populateTree("words.txt", tree)
-print_anagrams(tree, "POTS")
+                userWord = input("\nEnter the word you want to search: ")
+                print("\n\nThe anagrams fro the word '%s' are..." % (userWord))
+                print_anagram(userWord, tree) #check if the word has anagrams
+                
+            except FileNotFoundError:
+                print("\n\nOops! File not found!")
+                break
+            
+        print("\n\n\nFinding the max of anagams of file 'maxAnagrams'...")
+        try:
+            findMaxAnagrams("maxAnagrams.txt", tree)
+            break
+        except FileNotFoundError:
+            print("\n\nOops! File not found!")
+            break
+                     
+main()   
---
+++ treesRed.py
@@ -0,0 +1,220 @@
+'''red black tree was only modified a bit, but pretty much is 
+the same methods provided in ZyBooks
+'''
+class redBlackTree:
+    root = None
+    def __init__(self):
+        self.root = None
+    
+    def __len__(self):
+        if self.root is None:
+            return 0
+        return self.root.count()
+    
+    def insert(self, key):
+        new_node = RBTNode(key, None, True, None, None)
+        self.insert_node(new_node)
+        
+    def insert_node(self, node):
+        # Begin with normal BST insertion
+        if self.root is None:
+            # Special case for root
+            self.root = node
+        else:
+            current_node = self.root
+            while current_node is not None:
+                if node.key < current_node.key:
+                    if current_node.left is None:
+                        current_node.set_child("left", node)
+                        break
+                    else:
+                        current_node = current_node.left
+                else:
+                    if current_node.right is None:
+                        current_node.set_child("right", node)
+                        break
+                    else:
+                        current_node = current_node.right
+        # Color the node red
+        node.color = "red"
+        # Balance
+        self.insertion_balance(node)
+
+    def insertion_balance(self, node):
+        # If node is the tree's root, then color node black and return
+        if node.parent is None:
+            node.color = "black"
+            return
+        
+        # If parent is black, then return without any alterations
+        if node.parent.is_black():
+            return
+    
+        # References to parent, grandparent, and uncle are needed for remaining operations
+        parent = node.parent
+        grandparent = node.get_grandparent()
+        uncle = node.get_uncle()
+        
+        # If parent and uncle are both red, then color parent and uncle black, color grandparent
+        # red, recursively balance  grandparent, then return
+        if uncle is not None and uncle.is_red():
+            parent.color = uncle.color = "black"
+            grandparent.color = "red"
+            self.insertion_balance(grandparent)
+            return
+
+        # If node is parent's right child and parent is grandparent's left child, then rotate left
+        # at parent, update node and parent to point to parent and grandparent, respectively
+        if node is parent.right and parent is grandparent.left:
+            self.rotate_left(parent)
+            node = parent
+            parent = node.parent
+        # Else if node is parent's left child and parent is grandparent's right child, then rotate
+        # right at parent, update node and parent to point to parent and grandparent, respectively
+        elif node is parent.left and parent is grandparent.right:
+            self.rotate_right(parent)
+            node = parent
+            parent = node.parent
+
+        # Color parent black and grandparent red
+        parent.color = "black"
+        grandparent.color = "red"
+                
+        # If node is parent's left child, then rotate right at grandparent, otherwise rotate left
+        # at grandparent
+        if node is parent.left:
+            self.rotate_right(grandparent)
+        else:
+            self.rotate_left(grandparent)
+
+    def rotate_left(self, node):
+        right_left_child = node.right.left
+        if node.parent != None:
+            node.parent.replace_child(node, node.right)
+        else: # node is root
+            self.root = node.right
+            self.root.parent = None
+        node.right.set_child("left", node)
+        node.set_child("right", right_left_child)
+
+    def rotate_right(self, node):
+        left_right_child = node.left.right
+        if node.parent != None:
+            node.parent.replace_child(node, node.left)
+        else: # node is root
+            self.root = node.left
+            self.root.parent = None
+        node.left.set_child("right", node)
+        node.set_child("left", left_right_child)
+    
+    def search(self, key):
+        current_node = self.root
+        while current_node != None:
+            # Return the node if the key matches.
+            if(current_node.key == key):
+                return current_node #current_node
+                
+            # Navigate to the left if the search key is
+            # less than the node's key.
+            elif key < current_node.key:
+                current_node = current_node.left
+    
+            # Navigate to the right if the search key is
+            # greater than the node's key.
+            else:
+                current_node = current_node.right
+        # The key was not found in the tree.
+        return None
+        
+# RBTNode class - represents a node in a red-black tree
+class RBTNode:
+    key = ""
+    def __init__(self, key, parent, is_red = False, left = None, right = None):
+        self.key = key
+        self.left = left
+        self.right = right
+        self.parent = parent
+        
+        if is_red:
+            self.color = "red"
+        else:
+            self.color = "black"
+
+    # Returns true if both child nodes are black. A child set to None is considered
+    # to be black.
+    def are_both_children_black(self):
+        if self.left != None and self.left.is_red():
+            return False
+        if self.right != None and self.right.is_red():
+            return False
+        return True
+
+    def count(self):
+        count = 1
+        if self.left != None:
+            count = count + self.left.count()
+        if self.right != None:
+            count = count + self.right.count()
+        return count
+    
+    # Returns the grandparent of this node
+    def get_grandparent(self):
+        if self.parent is None:
+            return None
+        return self.parent.parent
+
+    # Gets this node's predecessor from the left child subtree
+    # Precondition: This node's left child is not None
+    def get_predecessor(self):
+        node = self.left
+        while node.right is not None:
+            node = node.right
+        return node
+
+    # Returns this node's sibling, or None if this node does not have a sibling
+    def get_sibling(self):
+        if self.parent is not None:
+            if self is self.parent.left:
+                return self.parent.right
+            return self.parent.left
+        return None
+
+    # Returns the uncle of this node
+    def get_uncle(self):
+        grandparent = self.get_grandparent()
+        if grandparent is None:
+            return None
+        if grandparent.left is self.parent:
+            return grandparent.right
+        return grandparent.left
+
+    # Returns True if this node is black, False otherwise
+    def is_black(self):
+        return self.color == "black"
+
+    # Returns True if this node is red, False otherwise
+    def is_red(self):
+        return self.color == "red"
+
+    # Replaces one of this node's children with a new child
+    def replace_child(self, current_child, new_child):
+        if self.left is current_child:
+            return self.set_child("left", new_child)
+        elif self.right is current_child:
+            return self.set_child("right", new_child)
+        return False
+
+    # Sets either the left or right child of this node
+    def set_child(self, which_child, child):
+        if which_child != "left" and which_child != "right":
+            return False
+            
+        if which_child == "left":
+            self.left = child
+        else:
+            self.right = child
+
+        if child != None:
+            child.parent = self
+
+        return True
